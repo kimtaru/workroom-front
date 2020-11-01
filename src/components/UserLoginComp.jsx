d67be5a97f608f6aabc15e445727bc6419e5fdc5
jinsol <jinsol@jinsolui-MacBookAir.local>
2020-11-01 22:35:30 +0900

    로그인 컴포넌트 생성 & 공통 scss 파일 생성

---
+++ src/components/UserLoginComp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UserLoginComp() {
+  return (
+    <div id="UserLoginComp">
+      <h1>UserLoginComp</h1>
+    </div>
+  );
+}
+
+export default UserLoginComp;
