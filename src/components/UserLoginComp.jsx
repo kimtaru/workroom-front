946a892fc4a64ae2c0af03e519dde9938c6f0881
kimtaru2 <kimtaru2@gmail.com>
2020-11-02 17:26:37 +0900

    개발환경구축(디펜던시 세팅 및 PACKAGE.JSON 설정)

--- src/components/UserLoginComp.jsx
+++ src/components/UserLoginComp.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React from 'react';
 
 function UserLoginComp() {
   return (
     <div id="UserLoginComp">
       <h1>UserLoginComp</h1>
+      <input type="text" className="input_basic" />
+      <input type="text" className="input_basic" />
+      <button className="btn_basic">LOGIN</button>
+      <button className="btn_basic">G</button>
     </div>
   );
 }
